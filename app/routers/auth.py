f7a32e04406ab48d5e7d8eff066e7173d8aec826
ceosteve <steven01njoroge@gmail.com>
2025-09-18 09:09:40 +0300

    modified the refresh endpoint to include token rotation

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -11,7 +11,8 @@ router = APIRouter(
 
 )
 
-@router.post("/login", response_model=schemas.Token)
+
+@router.post("/login", response_model=schemas.TokenOut)
 def login(user_credentials:OAuth2PasswordRequestForm= Depends(), db:Session=Depends(get_db)):
     
     query=db.query(models.Users).filter(models.Users.email==user_credentials.username)
@@ -20,7 +21,7 @@ def login(user_credentials:OAuth2PasswordRequestForm= Depends(), db:Session=Depe
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
     
-    if not utils.verify(user_credentials.password, user.password):
+    if not utils.verify_password(user_credentials.password, user.password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
 
     access_token = oauth2.create_access_token({"user_id":user.id})
@@ -32,22 +33,25 @@ def login(user_credentials:OAuth2PasswordRequestForm= Depends(), db:Session=Depe
 
 @router.post("/token/refresh", response_model=schemas.TokenOut)
 def refresh_token(request:schemas.RefreshRequest, db:Session=Depends(get_db)):
+
+    raw_refresh_token=request.refresh_token
+    hashed_token = utils.hash_token(raw_refresh_token)
+
+    db_token = db.query(models.RefreshToken).filter(models.RefreshToken.hashed_token==hashed_token
+                                                    ).filter(models.RefreshToken.expires_at > datetime.utcnow()).first()
     
-    raw= request.refresh_token
-    hashed= utils.hash_token(raw)
-
-    db_token=db.query(models.RefreshToken).filter(models.RefreshToken.hashed_token==hashed).filter(models.RefreshToken.expires_at > datetime.utcnow()).first()
-
     if not db_token:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="invalid refresh token")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid access token")
     
-    # rotate refresh token
+    #rotate refresh token
     user_id = db_token.user_id
     db.delete(db_token)
     db.commit()
-    new_raw_refresh = utils.make_refresh_record(db, user_id=user_id, days=7)
+
+    new_raw_refresh_token = utils.make_refresh_record(db,user_id=user_id, days=7)
+
+    new_access_token = oauth2.create_access_token({"user_id": user_id})
+
+    return {"acess_token":new_access_token, "token_type":"bearer", "refresh_token":new_raw_refresh_token}
 
 
-    access_token = oauth2.create_access_token({"data":user_id})
-    
-    return {"access_token": access_token, "token_type": "bearer", "refresh_token":new_raw_refresh}
