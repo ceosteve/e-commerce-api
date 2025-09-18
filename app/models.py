ea9e7735a6122c0c0399a514b9d68798f4764b8b
ceosteve <steven01njoroge@gmail.com>
2025-09-18 11:54:07 +0300

    added gender and birth date columns in the users table

--- app/models.py
+++ app/models.py
@@ -1,20 +1,29 @@
 
+import enum
 from .database import Base
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, TIMESTAMP
+from sqlalchemy import Column, Date, DateTime, Enum, ForeignKey, Integer, String, TIMESTAMP
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.expression import text
 
 
 
+
+class GenderEnum(str,enum.Enum):
+    M = "M"
+    F = "F"
 # users sqlalchemy model
 class Users(Base):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, index=True)
     first_name = Column(String, nullable=False)
     last_name = Column(String, nullable = False)
     email = Column(String, nullable=False, unique=True)
+    gender = Column(Enum(GenderEnum), nullable=False)
+    birth_date= Column(Date,nullable=False)
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    
+
 
     refresh_tokens = relationship("RefreshToken", back_populates="user", cascade="all, delete")
 
