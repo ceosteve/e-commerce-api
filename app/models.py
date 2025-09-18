60c4d2753506402ab6b646b4a3c8562ff0febefc
ceosteve <steven01njoroge@gmail.com>
2025-09-18 09:13:47 +0300

    modified Users sqlalchemy model

--- app/models.py
+++ app/models.py
@@ -12,14 +12,10 @@ class Users(Base):
     id = Column(Integer, primary_key=True)
     first_name = Column(String, nullable=False)
     last_name = Column(String, nullable = False)
-    email = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
 
-    @property 
-    def full_name(self):
-        return f"{self.fist_name} {self.last_name}"
-    
     refresh_tokens = relationship("RefreshToken", back_populates="user", cascade="all, delete")
 
 class RefreshToken(Base):
