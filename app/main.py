d08019ce27adfa367eb28ee7e525b61936120e6b
ceosteve <steven01njoroge@gmail.com>
2025-09-18 09:12:34 +0300

    included authentication router in the main application file

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import fastapi
 from fastapi import APIRouter, FastAPI, Depends
 
 from app import models
+from app.routers import auth
 
 from .database import Base, engine
 from .routers import users
@@ -14,6 +15,7 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(users.router)
+app.include_router(auth.router)
 
 @app.get("/")
 def root():
