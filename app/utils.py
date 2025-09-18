850408cdff0049f25c4477f9c543fd1ef872951d
ceosteve <steven01njoroge@gmail.com>
2025-09-18 09:18:31 +0300

    created a function to make a record of the refresh token in the database

--- app/utils.py
+++ app/utils.py
@@ -1,17 +1,18 @@
 
-
 from datetime import datetime, timedelta
 import secrets
+from fastapi import Depends
 from passlib.context import CryptContext
+from app.config import settings
 
-from app import models, schemas
+from app import config, models, schemas
 
 pswd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def hash(password:str):
+def hash_password(password:str) -> str:
     return pswd_context.hash(password)
 
-def verify(plain_password, hashed_password):
+def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pswd_context.verify(plain_password, hashed_password)
 
 
@@ -27,7 +28,8 @@ def verify_token(token:str, hashed_token:str) -> bool:
     return pswd_context.verify(token, hashed_token)
 
 
-def make_refresh_record(db, user_id:int, days: int = 7):
+# creating new record of refresh token in the database by calling the generate_raw_refresh_token function
+def make_refresh_record(db, user_id:int, days: int=settings.refresh_token_expiration_days):
     raw = generate_raw_refresh_token()
     hashed = hash_token(raw)
     expires= datetime.utcnow()+ timedelta(days=days)
