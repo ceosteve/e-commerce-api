27234f68ca17f1d4a8f133501e96f298b6f04c1d
ceosteve <steven01njoroge@gmail.com>
2025-09-19 16:52:35 +0300

    added a function to display exceptions in consistent format

--- app/utils.py
+++ app/utils.py
@@ -1,12 +1,16 @@
 
 
+from fastapi import HTTPException, status
 from passlib.context import CryptContext
 from app.config import settings
 
-from app import config, models, schemas
+from .errors import ERRORS
 
 pswd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+
+
+#  password hashing and verification
 def hash_password(password:str) -> str:
     return pswd_context.hash(password)
 
@@ -15,9 +19,36 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 
 
 
+# token hashing and verification
 def hash_token(token:str)-> str:
     return pswd_context.hash(token)
 
 def verify_token(token:str, hashed_token:str) -> bool:
     return pswd_context.verify(token, hashed_token)
 
+
+def raise_api_error(error_code:str, **kwargs):
+    error=ERRORS.get(error_code)
+
+    if not error:
+        raise HTTPException(
+            status_code= status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail={
+                "error":{
+                    "code":"INTERNAL_SERVER_ERROR",
+                    "message":f"unkown error code : {error_code}"
+                }
+            }
+        )
+    
+    message = error["message"].format(**kwargs) if kwargs else error["message"]
+
+    raise HTTPException(
+        status_code=error["status_code"],
+        detail={
+            "error":{
+                "code":error_code,
+                "message":message
+            }
+        }
+    )
