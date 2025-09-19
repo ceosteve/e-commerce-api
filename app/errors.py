496aabf8d8022751e1e97e595dfbabb3d52d4e6d
ceosteve <steven01njoroge@gmail.com>
2025-09-19 16:51:07 +0300

    created a file to handle HTTP exceptions and status coded

---
+++ app/errors.py
@@ -0,0 +1,20 @@
+ERRORS = {
+    "PRODUCT_NOT_FOUND":{
+        "status_code":404,
+        "message":"Product with id {id} not found"
+    },
+    "USER_NOT_FOUND":{
+        "status_code":404,
+        "message":"User with id {id} not found"
+    },
+    "FORBIDDEN":{
+        "status_code":403,
+        "message":"You do not have permission to perform this action"
+
+    },
+    "INVALID_CREDENTIALS":{
+        "status_code":401,
+        "message":"Invalid username or password"
+    }
+}
+
