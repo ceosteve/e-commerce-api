d13ddb3a0a6e5aaadfc5a08d51e1c457357c6583
ceosteve <steven01njoroge@gmail.com>
2025-09-20 13:59:37 +0300

    added errors for insufficient stock and missing orders

--- app/errors.py
+++ app/errors.py
@@ -12,9 +12,22 @@ ERRORS = {
         "message":"You do not have permission to perform this action"
 
     },
+
     "INVALID_CREDENTIALS":{
         "status_code":401,
         "message":"Invalid username or password"
+    },
+
+    "INSUFFICIENT_STOCK":{
+        "status_code":400,
+        "message":"insufficient stock"
+    },
+
+    "ORDER_NOT_FOUND":{
+        "status_code":404,
+        "message":"no orders found"
     }
+    
 }
+
 
