e62268fcc19f2de387f732fda8cc72ae57619871
ceosteve <steven01njoroge@gmail.com>
2025-09-20 13:58:50 +0300

    created an orders file for CRUD operations

---
+++ app/routers/orders.py
@@ -0,0 +1,99 @@
+
+from fastapi import APIRouter, status, Depends
+
+from sqlalchemy.orm import Session
+from typing import List
+from app.database import get_db
+from app.utils import raise_api_error
+from .. import models, schemas
+from .. import dependencies
+from datetime import datetime
+
+
+router = APIRouter(
+    prefix="/orders",
+    tags=['orders']
+)
+
+
+
+# user to create an order
+@router.post("/create/",status_code=status.HTTP_201_CREATED, response_model=schemas.OrderOut)
+def create_order(order:schemas.CreateOrder, 
+                 db:Session=Depends(get_db), 
+                 current_user:int=Depends(dependencies.get_current_user)):
+    try:
+        new_order = models.Order(
+            user_id=current_user.id,
+            placed_on= datetime.utcnow(),
+            status="pending",
+            total_price=0
+        )
+
+        db.add(new_order)
+        db.flush()
+
+        total_price = 0
+        order_items = []
+
+        for item in order.items:
+
+            product_exists= db.query(models.Products).filter(models.Products.id==item.product_id).first()
+
+            if not product_exists:
+                raise raise_api_error("PRODUCT_NOT_FOUND", id=item.product_id)
+
+            if product_exists.stock<item.item_quantity:
+                raise raise_api_error("INSUFFICIENT_STOCK")
+        
+            product_exists.stock -= item.item_quantity
+
+            new_order_item = models.OrderItem(
+                order_id=new_order.id,
+                product_id = item.product_id,
+                item_quantity = item.item_quantity,
+                unit_price = product_exists.price
+
+            )
+
+            db.add(new_order_item)
+            order_items.append(new_order_item)
+
+
+            total_price+=new_order_item.unit_price*item.item_quantity
+
+        new_order.total_price=total_price
+        db.commit()
+        db.refresh(new_order)
+
+        return{
+            "order_id": new_order.id,
+            "user_id": new_order.user_id,
+            "status": new_order.status,
+            "placed_on": new_order.placed_on,
+            "total_price": new_order.total_price,
+            "items": order_items
+        }
+
+    except Exception as e:
+        db.rollback()
+        raise e
+
+
+
+# view orders
+@router.get("/",status_code=status.HTTP_200_OK, response_model=List[schemas.OrderOut])
+def get_orders(db:Session=Depends(get_db), current_user:int=Depends(dependencies.get_current_user)):
+
+    orders=db.query(models.Order).filter(models.Order.user_id==current_user.id).all()
+
+    if not orders:
+        raise raise_api_error("ORDER_NOT_FOUND")
+    
+    return orders
+
+
+
+    
+
+    
