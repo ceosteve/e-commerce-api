3104551d0f9cb3707a1f3647c7ded4fc08ee541b
ceosteve <steven01njoroge@gmail.com>
2025-09-22 12:09:15 +0300

    created endpoints for updating orders and status

--- app/routers/orders.py
+++ app/routers/orders.py
@@ -1,4 +1,6 @@
 
+from logging import info
+from math import prod
 from fastapi import APIRouter, status, Depends
 
 from sqlalchemy.orm import Session
@@ -93,7 +95,76 @@ def get_orders(db:Session=Depends(get_db), current_user:int=Depends(dependencies
     return orders
 
 
+# update order items
+@router.put("/update/{id}/items", status_code=status.HTTP_200_OK, response_description=schemas.OrderOut)
+def update_order_item(id:int, update_data:schemas.OrderItemUpdate, db:Session=Depends(get_db), 
+                      current_user:int=Depends(dependencies.get_current_user)):
+
+    query=db.query(models.Order).filter(models.Order.id==id)
+    order = query.first()
+
+    if not order:
+        raise raise_api_error("ORDER_ID_NOT_FOUND", id=id)
+    
+     
+    if order.status != models.OrderStatus.pending:
+        raise raise_api_error("ORDER_ALREADY_FINALIZED")
+
+
+    for item in update_data.items:
+         product = db.query(models.Products).filter(models.Products.id==item.product_id).first()
+         if not product:
+             raise raise_api_error("PRODUCT_NOT_FOUND", id=item.product_id)
+         
+
+         order_item = db.query(models.OrderItem).filter(models.OrderItem.order_id==order.id).first()
+         if not order_item:
+             raise raise_api_error("ORDER_ID_NOT_FOUND", id=item.product_id)
+        
+         if product.stock < item.item_quantity:
+             raise raise_api_error("INSUFFICIENT STOCK")
+         
+        
+         product.stock -= (item.item_quantity- order_item.item_quantity)
+         order_item.item_quantity = item.item_quantity
+         order_item.unit_price = product.price
+    
+    order.recalc_total()
+
+    db.add(order)
+    db.commit()
+    db.refresh(order)
+
+    return order
+
+
+#  update order status
+@router.put("/update/{id}/status", status_code=status.HTTP_200_OK, response_model=schemas.OrderOut)
+def update_order_status(id:int, update_data:schemas.OrderStatusUpdate, 
+                        db:Session=Depends(get_db),
+                        current_user:models.UserRole=Depends(dependencies.require_admin)):
+
+
+    query=db.query(models.Order).filter(models.Order.id==id)
+    order = query.first()
+
+    if not order:
+        raise raise_api_error("ORDER_ID_NOT_FOUND", id=id)
+    
+    
+    order.status = update_data.status
+
+
+    db.commit()
+    db.refresh(order)
+
+    return order
+
+
+
+
+
+
 
     
-
     
