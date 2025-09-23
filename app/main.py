a741265cc7ea9a873fc11c36749fcafe600b5f55
ceosteve <steven01njoroge@gmail.com>
2025-09-23 14:54:29 +0300

    included logging configuration in main app entrypoint

--- app/main.py
+++ app/main.py
@@ -1,18 +1,37 @@
-
+from starlette.middleware.authentication import AuthenticationMiddleware
 import fastapi
-from fastapi import APIRouter, FastAPI, Depends
+from fastapi import APIRouter, FastAPI, Depends, Request
 
 from app import models
 from app.routers import auth, orders
 
 from .database import  engine
 from .routers import users, products,cart
+from app.logging_config import setup_logging
+from app.logging_middleware import LoggingMiddleware
+from app.logging_context import user_id_ctx
+
+
+logger = setup_logging()
 
 
 models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
+
+app.add_middleware(LoggingMiddleware)
+
+
+@app.on_event("startup")
+def startup_event():
+    logger.info("Application starting up")
+
+@app.on_event("shutdown")
+def shutdown_event():
+    logger.info("Application shutting down")
+
+
 
 app.include_router(users.router)
 app.include_router(auth.router)
