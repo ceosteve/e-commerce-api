41d1e6f5488bd85b72bc051d5c23acec8ac9dc09
ceosteve <steven01njoroge@gmail.com>
2025-10-06 11:24:16 +0300

    included a CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,17 +1,17 @@
 from contextlib import asynccontextmanager
 from starlette.middleware.authentication import AuthenticationMiddleware
 import fastapi
-from fastapi import APIRouter, FastAPI, Depends, Request
+from fastapi import APIRouter, FastAPI, Depends, Request, Response
 
 from app import models
 from app.routers import auth, orders
 
 from .database import  engine
-from .routers import users, products,cart
+from .routers import users, products, cart
 from app.logging_config import setup_logging
 from app.logging_middleware import LoggingMiddleware
 from app.logging_context import user_id_ctx
-
+from fastapi.middleware.cors import CORSMiddleware
 
 logger = setup_logging()
 
@@ -30,6 +30,16 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(LoggingMiddleware)
 
+origins = ['http://localhost:8000']
+
+"""restrict which domains can make api calls and the requests they can send"""
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials= True,
+    allow_methods=["*"],
+    allow_headers = ["*"],
+)
 
 app.include_router(users.router)
 app.include_router(auth.router)
